4fd7a9497d019938a47b79a89492f23d800f7e7a
Stoil100 <toli100ilov@gmail.com>
2023-10-02 22:19:57 +0300

    changed index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
-);
+root.render(<App />);
