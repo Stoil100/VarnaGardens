c88341b002437ac8d8e89ff49ea35a7d29a78b66
Stoil100 <toli100ilov@gmail.com>
2023-10-04 22:55:39 +0300

    Ensured https link

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+if (window.location.protocol == 'http:') {        
+  window.location.href = window.location.href.replace('http:', 'https:');
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
