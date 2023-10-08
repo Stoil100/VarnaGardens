86305cbf33c1a3af8c208a792b86d872d7a5434b
Stoil100 <toli100ilov@gmail.com>
2023-10-08 12:59:24 +0300

    Fixed mobile view for the gallery

--- src/Pages/Gallery.tsx
+++ src/Pages/Gallery.tsx
@@ -3,10 +3,12 @@ import { Title } from "../Components/Title";
 import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Autoplay } from "swiper/modules";
+import { EffectCoverflow, Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { isMobile } from "react-device-detect";
 
 import result0 from "../Assets/Results/results0.jpg";
 import result1 from "../Assets/Results/results1.jpg";
@@ -17,29 +19,47 @@ import result5 from "../Assets/Results/results5.jpg";
 import result6 from "../Assets/Results/results6.jpg";
 import result7 from "../Assets/Results/results7.jpg";
 
-const images = [result0, result1, result2, result3, result4,result5,result6,result7];
+const images = [
+  result0,
+  result1,
+  result2,
+  result3,
+  result4,
+  result5,
+  result6,
+  result7,
+];
 
-const swiperParams = {
-  spaceBetween: -200,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: true,
-  },
-  effect: "coverflow",
-  grabCursor: true,
-  slidesPerView: 3,
-  centeredSlides: true,
-  centerInsufficientSlides: true,
-  loop: true,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 500,
-    modifier: 1,
-    slideShadows: true,
-  },
-  modules: [EffectCoverflow, Autoplay],
-};
+const swiperParams = isMobile
+  ? {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: true,
+    },
+    modules: [Pagination, Autoplay],
+  }:{
+      spaceBetween: -200,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      },
+      effect: "coverflow",
+      grabCursor: true,
+      slidesPerView: 3,
+      centeredSlides: true,
+      centerInsufficientSlides: true,
+      loop: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 500,
+        modifier: 1,
+        slideShadows: true,
+      },
+      modules: [EffectCoverflow, Autoplay],
+    };
 
 const FlexBox = styled(Box)`
   display: flex;
